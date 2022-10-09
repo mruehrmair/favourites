34d3ebb80fab2d1fa6e86ec986915bd2589e3824
Markus Rührmair <markus.ruehrmair@gmail.com>
2022-10-09 15:03:20 +0200

    Initial setup of entities, repo and tests in Favourites.Data

---
+++ src/Favourites/Favourites.Data.Tests/BookmarkRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using Favourites.Data.Services;
+
+namespace Favourites.Data.Tests
+{
+
+    internal class BookmarkRepositoryTests : AbstractTest
+    {
+        [Test]
+        public void DbContextIsNotNull()
+        {
+            Assert.That(DbContext, Is.Not.Null);
+        }
+
+        [Test]
+        public async Task GetBookmarks_BaseSeeding_ReturnsCorrectNumberOfObjects()
+        {
+            //Arrange
+            var sut = new BookmarkRepository(DbContext);
+            const int expectedNumberOfSeededObjects = 2;
+
+            //Act
+            var bookmarks = await sut.GetBookmarks();
+            
+            //Assert
+            Assert.That(bookmarks.Count(), Is.EqualTo(expectedNumberOfSeededObjects));
+        }
+
+    }
+}
---
+++ src/Favourites/Favourites.Data/Services/IBookmarkRepository.cs
@@ -0,0 +1,13 @@
+﻿using Favourites.Data.Entities;
+
+namespace Favourites.Data.Services
+{
+    public interface IBookmarkRepository
+    {
+        public Task<IEnumerable<Bookmark>> GetBookmarks();
+        
+        public Task<IEnumerable<Tag>> GetTags();
+
+        public Task CreateBookmark(Bookmark bookmark);
+    }
+}
