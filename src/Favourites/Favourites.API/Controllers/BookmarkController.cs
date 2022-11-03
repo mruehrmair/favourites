c9205adeb9bb144cd96df44354032250f1c056c4
Markus Rührmair <markus.ruehrmair@gmail.com>
2022-11-03 20:34:48 +0100

    Added api project

---
+++ src/Favourites/Favourites.API/Controllers/BookmarkController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Favourites.API.Controllers;
+
+[ApiController]
+public class BookmarkController : ControllerBase
+{
+    [HttpGet("api/bookmarks")]
+    public JsonResult GetBookmarks()
+    {
+        return new JsonResult(
+            new List<object>
+            {
+                new { Weblink = "https://google.com", Description = "Google" },
+                new { Weblink = "https://dndbeyond.com", Description = "DnD" }
+            }
+        );
+    }
+}
