fc2187a2bc073a41ffb93b0b5912ebf686ded130
Markus Rührmair <markus.ruehrmair@gmail.com>
2022-11-06 13:14:02 +0100

    Added tag controller

---
+++ src/Favourites/Favourites.API/Controllers/TagController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Favourites.API.Controllers;
+
+[ApiController]
+public class TagController : ControllerBase
+{
+    private readonly IList<string> _tags = new List<string>
+    {
+        ".net",
+        "rpg"
+    };
+    
+    [HttpGet("api/tags")]
+    public ActionResult<IEnumerable<string>> GetTags()
+    {
+        return Ok(_tags);
+    }
+}
--- src/Favourites/Favourites.API/Program.cs
+++ src/Favourites/Favourites.API/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(
+    options => { options.ReturnHttpNotAcceptable = true; });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,9 +23,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.Run();
