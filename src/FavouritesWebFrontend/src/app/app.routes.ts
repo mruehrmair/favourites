16bf41b50486eb7ddab139b72bc09074ece6a3a9
Markus Rührmair <Markus.Ruehrmair@grollmus.de>
2023-04-28 16:08:47 +0200

    added bookmarks service

---
+++ src/FavouritesWebFrontend/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+const root = 'localhost:80';
+
+export default {
+    loadAll: () => `${root}`,
+    //   removeArticleFromOrderList: (articleId: string) => `${root}/${articleId}`,
+    //   addArticle: () => `${root}`,
+    //   changeArticleNumber: (articleId: string) => `${root}/${articleId}/articleNumber`,
+    //   changeArticleDescription: (articleId: string) => `${root}/${articleId}/description`,
+    //   changeArticleManufacturer: (articleId: string) => `${root}/${articleId}/manufacturer`,
+    //   changeArticleQuantity: (articleId: string) => `${root}/${articleId}/quantity`,
+};
---
+++ src/FavouritesWebFrontend/src/app/bookmarks/bookmarks-service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import ROUTES from '../app.routes';
+import { IBookmark } from './bookmark';
+
+@Injectable()
+export class OtherDeviceArticleService {
+  constructor(private readonly httpClient: HttpClient) {}
+
+  loadAll(): Observable<IBookmark[]> {
+    return this.httpClient.get<IBookmark[]>(ROUTES.loadAll());
+  }
+
+//   addArticle(): Observable<Article[]> {
+//     return this.httpClient.post<Article[]>(ROUTES.addArticle(), null);
+//   }
+
+//   removeArticle(articleId: string): Observable<Article[]> {
+//     return this.httpClient.delete<Article[]>(ROUTES.removeArticleFromOrderList(articleId));
+//   }
+
+//   changeArticleNumber(articleId: string, articleNumber: string): Observable<Article> {
+//     return this.httpClient.put<Article>(ROUTES.changeArticleNumber(articleId), { articleNumber: articleNumber });
+//   }
+
+//   changeArticleDescription(articleId: string, articleDescription: string): Observable<Article> {
+//     return this.httpClient.put<Article>(ROUTES.changeArticleDescription(articleId), { description: articleDescription });
+//   }
+
+//   changeArticleManufacturer(articleId: string, articleManufacturer: string): Observable<Article> {
+//     return this.httpClient.put<Article>(ROUTES.changeArticleManufacturer(articleId), { manufacturer: articleManufacturer });
+//   }
+
+//   changeArticleQuantity(articleId: string, articleQuantity: number): Observable<Article> {
+//     return this.httpClient.put<Article>(ROUTES.changeArticleQuantity(articleId), { quantity: articleQuantity });
+//   }
+}
